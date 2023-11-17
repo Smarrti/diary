ee899f875aa465cae448518ea6ec222656e5483e
Sergey Bashkin <se.bashkin@ya.ru>
2023-11-17 22:34:48 +0400

    constants colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,10 @@
 export const defaultColors = {
+  primary: '#1308FE',
   background: ['#FFFFFF'],
   disabled: '#AAB2B7',
+
+  text: '#000000',
+  grayText: '#778087',
+
+  transparent: 'transparent',
 };
--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,1 +1,43 @@
 export const HorizontalPaddingScreen = 32;
+
+export const fontSizes = {
+  fs12: '12',
+  fs14: '14',
+  fs16: '16',
+  fs18: '18',
+  fs20: '20',
+  fs22: '22',
+  fs24: '24',
+  fs26: '26',
+  fs28: '28',
+  fs30: '30',
+  fs32: '32',
+  fs34: '34',
+};
+
+export const fontWeights = {
+  fw100: 100,
+  fw200: 200,
+  fw300: 300,
+  fw400: 400,
+  fw500: 500,
+  fw600: 600,
+  fw700: 700,
+  fw800: 800,
+  fw900: 900,
+};
+
+export const lineHeights = {
+  lh12: '12',
+  lh14: '14',
+  lh16: '16',
+  lh18: '18',
+  lh20: '20',
+  lh22: '22',
+  lh24: '24',
+  lh26: '26',
+  lh28: '28',
+  lh30: '30',
+  lh32: '32',
+  lh34: '34',
+};
