9949f1e8e0f52655fe7ad58fda6bbb695bd5ca24
Sergey Bashkin <se.bashkin@ya.ru>
2023-11-17 22:34:57 +0400

    ui text

---
+++ src/ui/text/index.tsx
@@ -0,0 +1,45 @@
+import React, {FC} from 'react';
+import {TextProps} from 'react-native';
+import styled from 'styled-components/native';
+import {fontSizes, fontWeights} from '../../styles/constants';
+import {defaultColors} from '../../styles/colors';
+
+interface Props extends TextProps {
+  children: string;
+  fontSize?: string;
+  color?: string;
+  fontWeight?: number;
+  lineHeight?: string;
+}
+
+export const Text: FC<Props> = ({
+  children,
+  fontSize = fontSizes.fs16,
+  color = defaultColors.text,
+  fontWeight = fontWeights.fw400,
+  lineHeight,
+  ...rest
+}) => {
+  return (
+    <StyledText
+      fontSize={fontSize}
+      color={color}
+      fontWeight={fontWeight}
+      lineHeight={lineHeight}
+      {...rest}>
+      {children}
+    </StyledText>
+  );
+};
+
+const StyledText = styled.Text<{
+  fontSize: string;
+  color: string;
+  fontWeight: number;
+  lineHeight?: string;
+}>`
+  color: ${({color}) => color};
+  font-size: ${({fontSize}) => fontSize}px;
+  font-weight: ${({fontWeight}) => fontWeight};
+  line-height: ${({lineHeight}) => (lineHeight ? `${lineHeight}px` : 'auto')};
+`;
